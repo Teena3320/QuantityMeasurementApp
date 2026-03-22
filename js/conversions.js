6255f8bcb324dbc443b46846151fb9af11e32902
Teena3320 <teena.sebastian2022@vitstudent.ac.in>
2026-03-22 15:55:29 +0530

    Coded UC8 to Console

--- js/conversions.js
+++ js/conversions.js
@@ -12,3 +12,16 @@ export function applyConversion(value, convObj) {
     const result = eval(expr);
     return parseFloat(result.toFixed(6));
 }
+export function compareValues(v1, u1, v2, u2, base1, base2) {
+    if (isNaN(v1) || isNaN(v2)) return "Invalid values — cannot compare";
+
+    if (base1 > base2) {
+        return `${v1} ${u1} is GREATER than ${v2} ${u2}`;
+    }
+
+    if (base1 < base2) {
+        return `${v1} ${u1} is LESS than ${v2} ${u2}`;
+    }
+
+    return `${v1} ${u1} is EQUAL to ${v2} ${u2}`;
+}
